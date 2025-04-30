a90c2d7d052663b4d40eb043068fbd086dd56d7e
Anas <153933457+anas7abdelghany@users.noreply.github.com>
2025-05-01 00:19:45 +0300

    Update main.py

--- Phase2/main.py
+++ Phase2/main.py
@@ -1,11 +1,10 @@
 import pygame
 from pygame.locals import *
-import matplotlib.pyplot as plt
+import sys  
 from environment import FlappyBirdEnv
 from algorithm import QLAgent
 from display import static, render
 
-# Game constants
 SW = 280
 SH = 511
 FPS = 32
@@ -13,14 +12,12 @@ FPS = 32
 def main():
     pygame.init()
     window = pygame.display.set_mode((SW, SH))
-    pygame.display.set_caption("AI PROJECT")
+    pygame.display.set_caption("Flappy Bird")
     clock = pygame.time.Clock()
     
     env = FlappyBirdEnv()
     agent = QLAgent()
     generation = 1
-    x = []
-    y = []
     
     static(window)
     
@@ -30,13 +27,8 @@ def main():
         while not done:
             for event in pygame.event.get():
                 if event.type == QUIT:
-                    plt.scatter(x, y)
-                    plt.xlabel("GENERATION/Number of Trials")
-                    plt.ylabel("SCORE")
-                    plt.title("Flappy Birds : AI Project")
-                    plt.show()
                     pygame.quit()
-                    exit()
+                    sys.exit() 
             
             x_prev, y_prev = state
             action = agent.choose_action(x_prev, y_prev)
@@ -48,8 +40,6 @@ def main():
             
             state = next_state
             if done:
-                x.append(generation)
-                y.append(info['score'])
                 generation += 1
                 print(f"Generation {generation}, Score: {info['score']}")
                 break
