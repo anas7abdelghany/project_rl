a3b9ae5fde78dd4b4d15015a44895e0f4e6f2d26
Anas <153933457+anas7abdelghany@users.noreply.github.com>
2025-05-01 00:20:47 +0300

    Update algorithm.py

--- Phase2/algorithm.py
+++ Phase2/algorithm.py
@@ -1,22 +1,17 @@
 import numpy as np
 
 class QLAgent:
-    def __init__(self):
-        # Initialize Q-Table
-        self.Q = np.zeros((7, 21, 2), dtype=float)
-        self.actions = [0, 1]  # 0: Do nothing, 1: Jump
+    def __init__(self, learning_rate=0.8, decay_factor=0.5, gamma=0.9):
+        self.Q = np.zeros((7, 21, 2), dtype=float)  
+        self.actions = [0, 1]  
+        self.learning_rate = learning_rate
+        self.decay_factor = decay_factor
+        self.gamma = gamma
 
     def choose_action(self, x, y):
-        # Choose action based on Q-values
         return 1 if self.Q[x][y][1] > self.Q[x][y][0] else 0
 
-    def update(self, x_prev, y_prev, action, reward, x_new, y_new):
-        # Update Q-Table
-        if action == 1:
-            self.Q[x_prev][y_prev][1] = 0.4 * self.Q[x_prev][y_prev][1] + (0.6) * (
-                reward + max(self.Q[x_new][y_new][0], self.Q[x_new][y_new][1])
-            )
-        else:
-            self.Q[x_prev][y_prev][0] = 0.4 * self.Q[x_prev][y_prev][0] + (0.6) * (
-                reward + max(self.Q[x_new][y_new][0], self.Q[x_new][y_new][1])
-            )
+    def update(self, x_prev, y_prev, action, reward, x_next, y_next):
+        max_future_q = max(self.Q[x_next][y_next])
+        current_q = self.Q[x_prev][y_prev][action]
+        self.Q[x_prev][y_prev][action] = (self.decay_factor * current_q + self.learning_rate * (reward + self.gamma * max_future_q))
